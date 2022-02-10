4aa19c8131ab37039e66e63ae9e16f6d2bca5e29
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-10 18:35:48 -0500

    Implementación de movimiento para las barras

--- main.js
+++ main.js
@@ -31,14 +31,21 @@
 
         this.kind = "rectangle";
 
+        this.speed = 10;
+
 
     }
 
     self.Bar.prototype={
         down: function(){
+            this.y += this.speed;
         },
 
         up: function(){
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: "+this.x+" y: "+this.y;
         }
     }
 
@@ -75,13 +82,26 @@
 
 })();
 
-window.addEventListener("load",main);
+var board = new Board(800,400);
+var bar = new Bar (20,100,40,100,board);
+var bar = new Bar (735,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView (canvas,board);
+
+document.addEventListener("keydown",function(ev){
+    
+    if(ev.keyCode ==38){
+        bar.up();
+    }else if(ev.keyCode==40){
+        bar.down();
+    }
+    console.log(bar.toString());
+});
+
+self.addEventListener("load",main);
 
 function main(){
-    var board = new Board(800,400);
-    var bar = new Bar (20,100,40,100,board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView (canvas,board);
+   
 
     board_view.draw();
 
