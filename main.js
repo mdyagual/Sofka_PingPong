e763e551511dce989e9fec6566531c5467a01d7d
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-10 18:19:54 -0500

    Creación de la primera barra

--- main.js
+++ main.js
@@ -12,8 +12,33 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
+        }
+    }
+
+})();
+
+(function (){
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this);
+
+        this.kind = "rectangle";
+
+
+    }
+
+    self.Bar.prototype={
+        down: function(){
+        },
+
+        up: function(){
         }
     }
 
@@ -26,16 +51,38 @@
         this.canvas.height = board.height;
         this.board = board;
         this.ctx=canvas.getContext("2d");
-
     }
+    self.BoardView.prototype ={
+        draw: function(){
+            for (var i=this.board.elements.length - 1; i >= 0; i--){
+                var elem = this.board.elements[i];
+                draw(this.ctx,elem);
+            }
+        }
+    }
+    //Dibujar los elementos
+    function draw(ctx,element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    break;
+            }
+        }
+        
+    }
+
+
 })();
 
 window.addEventListener("load",main);
 
-
-
 function main(){
     var board = new Board(800,400);
+    var bar = new Bar (20,100,40,100,board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView (canvas,board);
+
+    board_view.draw();
+
 }
